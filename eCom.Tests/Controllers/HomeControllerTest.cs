ccda59b179860a355a442d47417173707ee5109e
Fredrik Olsson <fredriko.olsson@gmail.com>
2018-02-05 23:02:55 +0100

    init commit

--- eCom.Tests/Controllers/HomeControllerTest.cs
+++ eCom.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using eCom.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using eCom;
-using eCom.Controllers;
+using System.Web.Mvc;
 
 namespace eCom.Tests.Controllers
 {
---
+++ eCom.Tests/LoginTest.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+
+using OpenQA.Selenium.Firefox;
+using Xunit;
+
+namespace eCom.Tests
+{
+    public class LoginTest
+    {
+        [Fact]
+        public void StartApplication()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://localhost:53973/");
+
+                //                IWebElement applicationButton = driver.FindElement(By.Id("startApplication"));
+
+                //                applicationButton.Click();
+
+                Assert.Equal("Home Page - My ASP.NET Application", driver.Title);
+            }
+        }
+
+        [Fact]
+        public void SubmitApplication()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://localhost:53973/Home/StartLoanApplication");
+
+                IWebElement firstNameInput = driver.FindElement(By.Id("FirstName"));
+                firstNameInput.SendKeys("Sarah");
+
+                driver.FindElement(By.Id("LastName")).SendKeys("Smith");
+
+
+                driver.FindElement(By.Id("Loan")).Click();
+
+
+                driver.FindElement(By.Name("TermsAcceptance")).Click();
+            }
+        }
+
+    }
+}
